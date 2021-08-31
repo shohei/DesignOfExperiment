6547fe5f95b476ac92b8b8b6cc2b8a50ea29d9e5
Shohei Aoki <shoaok@gmail.com>
2021-08-31 14:34:48 +0300

    update

--- anova.py
+++ anova.py
@@ -5,6 +5,8 @@ import pdb
 import sys
 import inspect
 import re
+from itertools import combinations
+import math
 
 def flatten(t):
     if not type(t)==type([]): #t is numpy array 
@@ -76,6 +78,7 @@ def one_way_anova(df):
            print("Significant difference among A (p=0.01)")
         else:
            print("Significant difference among A (p=0.05)")
+        multiple_comparison_bonferroni(data, V_E, pvalue=0.05)
     else:
            print("No significant difference among A")
 
@@ -208,9 +211,36 @@ def two_way_anova(df):
     #prinfo(V_A, V_B, V_AB, V_E)
     #prinfo(F_A, F_B, F_AB)
 
+def multiple_comparison_bonferroni(data, V_E, pvalue):
+    nA = len(data)
+    flatten_data = flatten(data)
+    N = len(flatten_data)
+    mu = sum(flatten_data)/N
+    means = [[]]*nA
+    for i in range(nA):
+        means[i] = sum(data[i])/len(data[i])
+
+    pvalue_prime = pvalue / (nA*(nA-1)/2)  
+    f_prime = N - nA 
+    # Critical value T0
+    # p-value is divided by 2 since TINV() function of Excel uses two-sided tail probability
+    # https://scipy-user.scipy.narkive.com/j7pH5qnd/how-does-scipy-stats-t-isf-work
+    T0 = scipy.stats.t.isf(pvalue_prime/2, f_prime) 
+
+    combi =  [x for x in combinations(range(nA),2)]
+    T = [[]]*len(combi)
+    for i,(u,v) in enumerate(combi):
+        n_u = len(data[u])
+        n_v = len(data[v])
+        T[i] = abs(means[u]-means[v])/math.sqrt(V_E*(1/n_u+1/n_v))
+        if T[i] < T0:
+            print(" A{} and A{} don't have a significant difference.".format(u,v))
+        else:
+            print(" A{} and A{} have a significant difference.".format(u,v))
+
 
 def check_normality(df, pvalue):
-    if scipy.stats.shapiro(df["A"]).pvalue < pvalue:
+    if scipy.stats.shapiro(df.value).pvalue < pvalue:
         print("The given data doesn't comply with the normal distribution.")
         print("Thus the multiple comparison test cannot be applied.")
         print("Use a non-parametric multiple comparison test instead.")
@@ -218,10 +248,9 @@ def check_normality(df, pvalue):
     else:
         return True
 
-def check_homoscedasticity(df, pvalue):
+def check_homoscedasticity_one_way(df, pvalue):
     nA = max(df["A"])
     valueA = [df[df["A"]==i].value.tolist() for i in range(1,nA+1)]
-    pdb.set_trace()
     if scipy.stats.bartlett(*valueA).pvalue < pvalue:
         print("The given data doesn't comply with the homoscedasticity.")
         print("Thus the multiple comparison test cannot be applied.")
@@ -231,20 +260,45 @@ def check_homoscedasticity(df, pvalue):
     else:
         return True
 
+def check_homoscedasticity_two_way(df, pvalue):
+    nA = max(df["A"])
+    nB = max(df["B"])
+    valueA = [df[df["A"]==i].value.tolist() for i in range(1,nA+1)]
+    valueB = [df[df["B"]==i].value.tolist() for i in range(1,nB+1)]
+    isHomoscedasticity = True
+    if scipy.stats.bartlett(*valueA).pvalue < pvalue:
+        print("The cateogry A doesn't comply with the homoscedasticity.")
+        print("Thus the multiple comparison test cannot be applied.")
+        print("Apply 1) Non-parametric test, 2) Kruskal-Wallis test,")
+        print("and finally 3) Non-parametric multiple comparison test.")
+        isHomoscedasticity = False
+    if scipy.stats.bartlett(*valueB).pvalue < pvalue:
+        print("The category B doesn't comply with the homoscedasticity.")
+        print("Thus the multiple comparison test cannot be applied.")
+        print("Apply 1) Non-parametric test, 2) Kruskal-Wallis test,")
+        print("and finally 3) Non-parametric multiple comparison test.")
+        isHomoscedasticity = False
+    return isHomoscedasticity
+
 if __name__=="__main__":
     if len(sys.argv)!=2:
         print("Usage: $ python anova.py <CSV_FILE.csv>")
         exit()
     csv = str(sys.argv[1])
     df = pd.read_csv(csv)
-    
-    if not check_normality(df, pvalue=0.01):
-        exit()
-    if not check_homoscedasticity(df, pvalue=0.05):
-        exit()
 
     if "B" in df.keys():
+        # Two way ANOVA
+        print("Two way ANOVA")
+        if not check_normality(df, pvalue=0.05) \
+           or not check_homoscedasticity_two_way(df, pvalue=0.05):
+            exit()
         two_way_anova(df)
     else:
+        # One way ANOVA
+        print("One way ANOVA")
+        if not check_normality(df, pvalue=0.05) \
+           or not check_homoscedasticity_one_way(df, pvalue=0.05):
+            exit()
         one_way_anova(df)
 
